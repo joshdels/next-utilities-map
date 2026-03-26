5bd8b364ec104e242b639976feb37d892af94f48
joshdels <deleonjoshy2@gmail.com>
2026-03-26 18:28:19 +0800

    feat: floating chevron collapsable

--- src/features/map/sidebar/FloatingDashboard.tsx
+++ src/features/map/sidebar/FloatingDashboard.tsx
@@ -1,26 +1,48 @@
-import { x24 } from "@esri/calcite-ui-icons";
+import { useState } from "react";
 import SideBar from "./SideBar";
+import { ChevronsRight, X } from "lucide-react";
 
 export default function FloatingDashboard() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div className="m-5 flex h-[90vh] w-150 flex-row gap-5 rounded-lg bg-white text-black">
-      <aside className="w-[20%] rounded-lg bg-gray-200 p-3">
+    <div
+      className={`m-5 flex h-[90vh] flex-row gap-5 rounded-lg bg-white text-black shadow-md transition-all duration-300 ease-in-out ${isOpen ? "w-150" : "w-30"}`}
+    >
+      <aside
+        className={`rounded-md p-3 ${isOpen ? "bg-gray-200" : "bg-white"} `}
+      >
         <SideBar />
       </aside>
 
-      <main className="flex-1 rounded-lg bg-white p-3">
+      <main
+        className={`flex-1 rounded-lg bg-white p-3 transition-all duration-300 ease-in-out ${isOpen ? "translate-x-0 opacity-100" : "pointer-events-none -translate-x-5 opacity-0"}`}
+      >
         <section className="mb-5 flex flex-row justify-between">
-          <h1 className="text-2xl font-black">Project Overview</h1>
-          <div className="flex flex-row items-center">
-            <div className="cursor-pointer hover:text-blue-600">
-              <svg className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
-                <path d={x24} />
-              </svg>
-            </div>
-          </div>
+          <h1 className="overflow-hidden text-2xl font-black whitespace-nowrap">
+            Project Overview
+          </h1>
+
+          <button onClick={toggleOpen}>
+            <X className="h-5 w-5 cursor-pointer hover:text-blue-500" />
+          </button>
         </section>
+
         <section>{/* <OwnerInformation /> */}</section>
       </main>
+
+      {!isOpen && (
+        <button
+          onClick={toggleOpen}
+          className="absolute top-2 right-0 rounded bg-gray-500 px-3 py-1 text-white transition duration-500 ease-in-out hover:bg-blue-600"
+        >
+          <ChevronsRight className="h-5 w-5 cursor-pointer" />
+        </button>
+      )}
     </div>
   );
 }
